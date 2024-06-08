cf29e8cd9ec4a2eb47e3af1c5583a47952dc5cd4
Sam Plews <the@codeite.com>
2024-06-08 19:10:02 +0100

    Templete UI

---
+++ ui/README.md
@@ -0,0 +1,1 @@
+A prototype UI for a language learning focused AI chat bot app.
---
+++ ui/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      Lang Chat
+      
+    </>
+  )
+}
+
+export default App
