bcb7189d8f4b853ff91a097ade32b37d29b115d1
Sam Plews <the@codeite.com>
2024-06-13 02:33:39 +0100

    Fix new chat

--- ui/src/pages/new-chat/NewChatPage.tsx
+++ ui/src/pages/new-chat/NewChatPage.tsx
@@ -1,22 +1,49 @@
-import { useEffect } from 'react'
+import styled from 'styled-components'
 import { useNavTo } from '../../helpers/navTo'
+import { useState } from 'react'
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`
+
+const RandomChatButton = styled.button`
+  color: var(--main-fg-color);
+  background-color: var(--accent-bg-color);
+  border: none;
+  border-radius: 4px;
+
+  cursor: pointer;
+  font-size: 32px;
+`
 
 export function NewChatPage() {
   const navTo = useNavTo()
+  const [buttonDisabled, setButtonDisabled] = useState(false)
 
-  useEffect(() => {
+  const createChat = () => {
+    setButtonDisabled(true)
     fetch('/api/start-chat', {
       method: 'GET',
     })
       .then((res) => res.json())
       .then((data) => {
-        navTo('Chat', data.chatId)
+        alert(JSON.stringify(data))
+        alert(data.chat.id)
+        navTo('Chat', data.chat.id)
       })
-  }, [navTo])
+      .finally(() => {
+        setButtonDisabled(false)
+      })
+  }
 
   return (
-    <div>
-      <h1>Starting New Chat</h1>
-    </div>
+    <ButtonContainer>
+      <RandomChatButton onClick={createChat} disabled={buttonDisabled}>
+        Start Random chat
+      </RandomChatButton>
+    </ButtonContainer>
   )
 }
